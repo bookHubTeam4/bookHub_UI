78fd7b301359a16ddf8c6bee0afb5a1888aee298
pawanvig <pawan1119vig@gmail.com>
2018-10-22 00:34:25 -0400

    initial request to google API

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,57 @@
 import React, { Component } from 'react';
 import Styles from './App.css';
 
+import Gallery from './Components/Gallery';
+
 class App extends Component {
+  constructor(props)
+    {
+      super(props);
+      this.state = {value :'hello',
+      items : []
+    };
+
+      this.handleChange = this.handleChange.bind(this);
+      this.search=this.search.bind(this);
+
+
+    }
+
+    search(){
+      let query = this.state.value;
+    const BASE_URL = "https://www.googleapis.com/books/v1/volumes?q=" + query;
+    fetch(BASE_URL, {method:"GET"})
+    .then(response =>  response.json())
+    .then(json => {
+      let {items} = json;
+
+      this.setState({items: items});
+    })
+    .then(console.log(this.state.items));
+    }
+
+    handleChange(event){
+      this.setState({
+        value: event.target.value
+      })
+    }
   render() {
+    
     return (
       <div className={Styles.App}>
+
+
         <p>BookHub</p>
+
+        <label>
+          Name:
+          <input type="text" placeholder="Search for a book" value={this.state.value} onChange= {this.handleChange} />
+          <input type="submit" value= "Submit"  onClick={this.search} />
+        </label>
+  <form>
+
+  </form>
+  <Gallery items={this.state.items} />
       </div>
     );
   }
---
+++ src/Components/Gallery.jsx
@@ -0,0 +1,30 @@
+import React  from 'react';
+//import {FormGroup, FormControl,InputGroup, Glyphicon} from 'react-bootstrap';
+
+export default class Gallery extends React.Component{
+    render(){
+        return (
+            <div>
+            {
+                this.props.items.map((item , i) => {
+                    let {title, imageLinks , infoLink} = item.volumeInfo
+                    return (
+                        <a href ={infoLink}
+                        target = "_blank"
+                        key={i} className = "book">
+                        <img 
+                        src ={imageLinks !== undefined? imageLinks.thumbnail : ''} 
+                        alt = "book image"
+                        className = "bookImage"
+                        />
+                        <div className = "titleText">{title }</div>
+
+                        </a>
+                        
+                    );
+                })
+            }</div>
+
+        );
+    }
+}
