c818f66f1c7991f73fd3ead3499a89dd3f4640fc
pawanvig <pawan1119vig@gmail.com>
2018-10-26 13:58:18 -0400

    book info page with react strap october 26

--- src/Components/BookInformation/BookInfo.jsx
+++ src/Components/BookInformation/BookInfo.jsx
@@ -1,20 +1,32 @@
 import React, { Component } from 'react';
-import { BookInfoService} from "../../Service/Services";
+import { BookInfoService } from "../../Service/Services";
+
+import {
+    Container,
+    Row,
+    Col,
+    Jumbotron,
+    Badge,
+    Button
+} from 'reactstrap';
+
+
+
+
 const queryString = require('query-string');
 
 
 
 export default class BookInfo extends Component {
 
-    constructor(props)
-    {
+    constructor(props) {
         super(props);
         this.state = {
-            items:[],
-            itm: []
-            
+            items: [],
+            flag: false
+
         }
-  
+
     }
 
 
@@ -23,77 +35,94 @@ export default class BookInfo extends Component {
 
         console.log(window.location.href) // "im"
         console.log(window.location.href.substr(window.location.href.lastIndexOf('/') + 1));
-       let isbn = window.location.href.substr(window.location.href.lastIndexOf('/') + 1);
-       BookInfoService(isbn)
-       .then( e => e.json()).then(e=>{
-           console.log(e)
-           this.setState({items:e})
-       })
-     
-       // this.setState({items : e.json()});
+        let isbn = window.location.href.substr(window.location.href.lastIndexOf('/') + 1);
+        BookInfoService(isbn)
+            .then(e => e.json()).then(e => {
+                console.log(e);
+                this.setState({ items: e });
+                console.log("this is item " + this.state.items.book.title);
+                this.setState({ flag: true });
+            })
+
+
         console.log("im in book info component");
-       // console.log(this.state.items);
-        //console.log(this.state.items.title);
-   // });
+
+
 
     }
 
-  /*  componentWillMount() {
-      //  const values = queryString.parse(this.props.location.search);
-      console.log("hellpp");
-      //const parsed = queryString.parse(this.props.location.search);
-        console.log(window.location.href) // "im"
-        console.log(window.location.href.substr(window.location.href.lastIndexOf('/') + 1));
-       let isbn = window.location.href.substr(window.location.href.lastIndexOf('/') + 1);
-        
-       console.log("isbn is "+ isbn);
-       const BASE_URL = "https://www.googleapis.com/books/v1/volumes?q="+isbn+"&key=AIzaSyCzRLuhzYmdLVsVinQopkmunyvI-ae6I6I";
-       
-       fetch(BASE_URL, {method:"GET"})
-       .then(response =>  response.json() )
-       .then(json => {
-
-         this.setState(
-             {
-            items :  json,
-             itm : json.items
-             }
-         );
-
-       // this.state.items = json;
-       // this.state.itm = json.items;
-        console.log("in fetch request");
-        console.log(this.state.itm[0].volumeInfo.industryIdentifiers[0]);
-        console.log(this.state.items);})
-
-     // console.log( "we are here "+this.state.items[0]);
-    
-
-      }   
- */
-
-
-  render() {
 
 
 
-    
-       
-    
+    render() {
 
-   /* if ( ! this.state.itm[0] == []) {
 
-    return (
-      <div>
-      <img src= {`${this.state.itm[0].volumeInfo.imageLinks.thumbnail}`} alt="book cover"/>
-      </div>
-    )
-    }  */
-   
+
+
+
+
+
+        if (this.state.flag) {
+
+            return (
+                <div>
+
+                    <Jumbotron>
+                        <Container>
+                            <Row>
+                                <Col>
+                                    <Badge color="primary"><h1>{this.state.items.book.title}</h1></Badge>
+
+                                </Col>
+                            </Row>
+                        </Container>
+                    </Jumbotron>
+
+                  
+                 
+
+                    <div class="row">
+
+
+                        <div class="col-md-2">
+                            <img width="150" height="150" src={`${this.state.items.book.image_url}`} />
+                        </div>
+                       
+
+                        <div class="col-md-10">
+
+                            <h5> {this.state.items.book.description}</h5>
+                        </div>
+
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <p>AUTHOR : {this.state.items.book.author}</p>
+                            <p>Published Date : {this.state.items.book.published_date}</p>
+                            <p>ISBN : {this.state.items.book.isbn}</p>
+                        </div>   </div>
+                        
+
+
+                    <Jumbotron align="center">
+                        <div>
+                            <Button color="primary">Add To Reading List</Button>{' '}
+                            <Button color="success">Finished Reading</Button>{' '}
+                            <Button color="danger">Can't Buy?</Button>{' '}
+                        </div>
+                    </Jumbotron>
+
+
+                </div>
+
+
+            );
+        }
+
         return (
             <div>Loading...</div>
         );
-    
-  
-}
+
+
+    }
 }
