cfb170c2e35ec18971186eded2e142dbeee4da3f
akhiljaz <akhiljaz@gmail.com>
2018-10-25 20:42:06 -0400

    Login and SIgnup Service and UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
 import SearchMain from "../src/Containers/SearchMain/SearchMain";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Authentication from '../src/Components/Authentication/Authentication';
 // import Styles from "./App.css";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
+      <Switch>
         <Route path="/" exact component={SearchMain} />
+        <Route path="/login" exact component={Authentication} />
+        </Switch>
       </BrowserRouter>
     );
   }
