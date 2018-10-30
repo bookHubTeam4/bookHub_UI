cfe55599e9d972d3f1e16d75ea11b11541f4d8c6
akhiljaz <akhiljaz@gmail.com>
2018-10-30 00:52:09 -0400

    sign in with fb and google

--- src/Components/Authentication/SignInForm.jsx
+++ src/Components/Authentication/SignInForm.jsx
@@ -1,71 +1,113 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import Style from '../Authentication/Authentication.css'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Style from "../Authentication/Authentication.css";
 import { loginService } from "../../Service/Services";
-import { GoogleLogin } from 'react-google-login';
+import { GoogleLogin } from "react-google-login";
+import  FacebookLogin  from "react-facebook-login";
 
 class SignInForm extends Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            email: '',
-            password: ''
-        };
+    this.state = {
+      email: "",
+      password: ""
+    };
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.responseGoogle = this.responseGoogle.bind(this);
-    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.responseGoogle = this.responseGoogle.bind(this);
+    this.responseFacebook = this.responseFacebook.bind(this);
+  }
 
-    handleChange(e) {
-        let target = e.target;
-        let value = target.value;
-        let name = target.name;
+  handleChange(e) {
+    let target = e.target;
+    let value = target.value;
+    let name = target.name;
 
-        this.setState({
-          [name]: value
-        });
-    }
+    this.setState({
+      [name]: value
+    });
+  }
 
-    handleSubmit(e) {
-        e.preventDefault();
+  handleSubmit(e) {
+    e.preventDefault();
 
-        loginService(this.state.email, this.state.password)
-        .then(e=>e.json()).then(e=>console.table(e))
-    }
+    loginService(this.state.email, this.state.password)
+      .then(e => e.json())
+      .then(e => console.table(e));
+  }
 
-    responseGoogle(response)
-    {
-        console.log(response);
-    }
+  responseGoogle(response) {
+    console.log(response);
+  }
 
-    render() {
-        return (
-        <div className={Style.FormCenter}>
-            <form onSubmit={this.handleSubmit} className="form-group" onSubmit={this.handleSubmit}>
-            <div className={Style.FormField}>
-                <input type="email" id="email" className="form-control" placeholder="Enter your email" name="email" onChange={this.handleChange} required />
-              </div>
+  responseFacebook = response => {
+    console.log(response);
+  };
 
-              <div className={Style.FormField}>
-                <input type="password" id="password" className="form-control" placeholder="Enter your password" name="password" onChange={this.handleChange} required/>
-              </div>
-
-              <div className={Style.FormField}>
-                  <button className="btn btn-primary">Sign In</button> <Link to="/signUp" className={Style.FormField__Link}>Create an account</Link>
-              </div>
-            </form>
-            <GoogleLogin
-                clientId="867945999343-gr16hdcap7et6keb5qf8lderkurtri27.apps.googleusercontent.com"
-                buttonText="Login"
-                onSuccess={this.responseGoogle}
-                onFailure={this.responseGoogle}
-                />
-
+  render() {
+    return (
+      <div className={Style.FormCenter}>
+        <form
+          onSubmit={this.handleSubmit}
+          className="form-group"
+          onSubmit={this.handleSubmit}
+        >
+          <div className={Style.FormField}>
+            <input
+              type="email"
+              id="email"
+              className="form-control"
+              placeholder="Enter your email"
+              name="email"
+              onChange={this.handleChange}
+              required
+            />
           </div>
-        );
-    }
+
+          <div className={Style.FormField}>
+            <input
+              type="password"
+              id="password"
+              className="form-control"
+              placeholder="Enter your password"
+              name="password"
+              onChange={this.handleChange}
+              required
+            />
+          </div>
+
+          <div className={Style.FormField}>
+            <button className="btn btn-primary">Sign In</button>{" "}
+            <Link to="/signUp" className={Style.FormField__Link}>
+              Create an account
+            </Link>
+          </div>
+        </form>
+        <div className="row">
+        <GoogleLogin 
+          clientId="867945999343-gr16hdcap7et6keb5qf8lderkurtri27.apps.googleusercontent.com"
+          onSuccess={this.responseGoogle}
+          onFailure={this.responseGoogle}
+        >
+          <i
+            style={{ color: "white" }}
+            className="fab fa-google"
+          />
+        </GoogleLogin>
+        <FacebookLogin 
+          appId="2187369724923695"
+          autoLoad
+          callback={this.responseFacebook}
+          render={renderProps => (
+            <button onClick={renderProps.onClick}>This is my custom FB button</button>
+          )}
+        />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default SignInForm;
